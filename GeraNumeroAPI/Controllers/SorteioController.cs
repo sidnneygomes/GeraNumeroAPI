5d7d3aebe89bc02df3ea9607b4b5abd49ed669e6
User <User@DESKTOP-81C45E3>
2023-03-11 16:49:54 -0300

    Add project files.

---
+++ GeraNumeroAPI/Controllers/SorteioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeraNumeroAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SorteioController : Controller
+    {
+        
+        [HttpGet]
+        public async Task<IEnumerable<int>> Get() =>
+             Enumerable.Range(1, 6).Select(index =>
+                Random.Shared.Next(1, 60)
+            ).ToArray().Order();
+    }
+}
